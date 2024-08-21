5c80fe85d0ca65cabac12ca22186378a2822e41b
kashifx6 <Tabkashifx2>
2024-08-21 20:08:23 +0500

    testing

--- src/app/simulation-summary/page.js
+++ src/app/simulation-summary/page.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import HeroSection from '../components/simulationSummary/heroSection/page'
 import AnalysisCard from '../components/simulationSummary/analysisCard/page'
-import CircleChart from '../components/simulationSummary/circleChart/page'
 import InvestmentCard from '../components/simulationSummary/investmentCard/page'
 import Footer from '../components/common/footer/page'
 import Header from '../components/common/header/page'
@@ -13,12 +12,10 @@ const SimulationSummary = () => {
             <Header />
             <HeroSection />
             <div className='lg:flex block p-4 my-6 gap-5'>
-                <div className='lg:w-1/2 w-full'>
+                <div className='w-full'>
                     <AnalysisCard />
                     <p className='text-2xl font-bold text-btnColor my-14'>Distribution of interest and capital</p>
-                    <div className='h-60 bg-white shadow-2xl border p-2 rounded-md'>
-                        <CircleChart />
-                    </div>
+                   
                 </div>
                 <div className='lg:w-1/2 w-full'>
                     <InvestmentCard />
