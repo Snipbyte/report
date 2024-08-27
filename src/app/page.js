8dc2415cb69b7f7c8b81600630329486f4ad1f00
kashifx6 <Tabkashifx2>
2024-08-27 09:46:17 +0500

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
          <p className="text-3xl text-headingColor font-bold text-center w-[450px] mx-auto mt-6 mb-2">A single time tracking app for desktops and mobile phones.</p>
         <p className="text-xs text-paraColor text-center">A single time tracker and timesheet app that lets you track work hours across projects.</p>
         <div className="flex my-4 items-center justify-center gap-8">
-           <ImageCard img="/images/image3.jpg" heading="Collaboration Teams" des="You can handle project together with team easily." />
+          <ImageCard img="/images/image3.jpg" heading="Collaboration Teams" des="You can handle project together with team easily." />
           <ImageCard img="/images/image1.jpg" heading="Cloud Storage" des="You can handle project together with team easily." />
           <ImageCard img="/images/image2.jpg" heading="Data Analytic" des="You can handle project together with team easily." />
         </div>
