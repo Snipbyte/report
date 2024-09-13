ee65c061e90ed31383c9bae4386661fbd9bc5482
kashifx6 <Tabkashifx2>
2024-09-13 15:48:26 +0500

    test

--- src/app/components/landingpage/heroSection/page.js
+++ src/app/components/landingpage/heroSection/page.js
@@ -7,7 +7,7 @@ const HeroSection = () => {
             <div className='w-full lg:w-[50%]'>
                 <Image className='w-full lg:h-[500px] h-[300px]' src="/images/lp1.jpg" width={1000} height={1000} />
             </div>
-            <div className='w-full  lg:w-[50%] bg-white shadow-2xl p-10'>
+            <div className='w-full  lg:w-[50%] bg-white  p-10'>
                 <p className='text-4xl font-bold text-headingColor my-6'>Boost Your Funding Prospects with IziKemp!</p>
                 <p className='text-headingColor text-lg my-8'>Access cutting-edge simulators and instantly generate personalized reports to assess your eligibility with banks and investment funds, accompanied by our expert guidance.</p>
             </div>
