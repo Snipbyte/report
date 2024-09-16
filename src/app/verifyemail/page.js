14c7e1702346b49dc1ae28a28443f5396fda6a3c
kashifx6 <Tabkashifx2>
2024-09-16 22:39:03 +0500

    susponse add

--- src/app/verifyemail/page.js
+++ src/app/verifyemail/page.js
@@ -1,18 +1,17 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation'
+import React, { useEffect, useState, Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 const VerifyEmail = () => {
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const searchParams = useSearchParams();
-  const token = searchParams.get('token')
-  console.log(token)
+  const token = searchParams.get('token');
+  
   useEffect(() => {
     const verifyEmail = async () => {
-
       if (!token) {
         setMessage('Invalid verification link.');
         setLoading(false);
@@ -41,7 +40,7 @@ const VerifyEmail = () => {
     if (token) {
       verifyEmail();
     }
-  }, []);
+  }, [token]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -50,4 +49,10 @@ const VerifyEmail = () => {
   return <div>{error ? error : message}</div>;
 };
 
-export default VerifyEmail;
+const VerifyEmailWithSuspense = () => (
+  <Suspense fallback={<div>Loading verification process...</div>}>
+    <VerifyEmail />
+  </Suspense>
+);
+
+export default VerifyEmailWithSuspense;
