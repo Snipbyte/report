9f2e16677a9539cbb6a152ccea22975ed25df992
kashifx6 <Tabkashifx2>
2024-09-17 12:40:38 +0500

    arrow fix

--- src/app/components/calculator/businessContent/page.js
+++ src/app/components/calculator/businessContent/page.js
@@ -5,10 +5,13 @@ import { IoIosArrowForward } from "react-icons/io";
 const BusinessContent = (props) => {
   return (
     <div className="w-full p-1">
-      <div className="flex items-center justify-between cursor-pointer text-headingColor hover:text-hoverBtnColor w-full bg-white shadow-md border hover:border-l-4 duration-500 hover:border-l-hoverBtnColor p-6 rounded-md">
-        <Link href="/calculator-detail/calculator">{props.info}</Link>
+      <Link
+        href="/calculator-detail/calculator"
+        className="flex items-center justify-between cursor-pointer text-headingColor hover:text-hoverBtnColor w-full bg-white shadow-md border hover:border-l-4 duration-500 hover:border-l-hoverBtnColor p-6 rounded-md"
+      >
+        <div>{props.info}</div>
         <IoIosArrowForward className="w-6 h-6" />
-      </div>
+      </Link>
     </div>
   );
 };
