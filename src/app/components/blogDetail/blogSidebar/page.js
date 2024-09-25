9c133bd38697f4fb3f68b27401e67721ccd932ed
kashifx6 <Tabkashifx2>
2024-09-25 17:18:58 +0500

    tags fix

--- src/app/components/blogDetail/blogSidebar/page.js
+++ src/app/components/blogDetail/blogSidebar/page.js
@@ -1,18 +1,21 @@
-// BlogSidebar.js
 import React from "react";
 
-const BlogSidebar = ({ tags }) => {
+const BlogSidebar = ({ tags = [] }) => { // Default value for tags
   return (
     <div>
       <p className="text-headingColor text-xl font-bold mt-8 mb-4">Tags</p>
-      {tags.map((tag, index) => (
-        <p
-          key={index}
-          className="text-btnColor hover:text-hoverBtnColor underline mt-2 cursor-pointer"
-        >
-          {tag}
-        </p>
-      ))}
+      {Array.isArray(tags) && tags.length > 0 ? ( // Check if tags is an array
+        tags.map((tag, index) => (
+          <p
+            key={index}
+            className="text-btnColor hover:text-hoverBtnColor underline mt-2 cursor-pointer"
+          >
+            {tag}
+          </p>
+        ))
+      ) : (
+        <p className="text-gray-400">No tags available</p> // Fallback if no tags
+      )}
     </div>
   );
 };
