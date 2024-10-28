136de971ddc07592896d8b17e489dbaa2a035e9b
Snipbyte <snipbytesol@gmail.com>
2024-10-28 19:40:55 +0500

    test api

--- src/app/pricingplan/page.js
+++ src/app/pricingplan/page.js
@@ -9,7 +9,7 @@ const PricingPlan = () => {
       <Header />
       <div className="flex flex-wrap justify-center items-center">
         <PricingCards
-          num="€29.70"
+          num="€9.90"
           des="Starter"
           point1="Report with the possibility of making up to three modifications"
           point2="Access to an online consultant to assist you in presenting your application"
@@ -18,7 +18,7 @@ const PricingPlan = () => {
         />
         {/* <PricingCards num="$39" des="Base" point1="Everything in Simple" point2="512GB of buisness storage" point3="Unlimited management" point4="Unlimited collaboration" point5="Links with password protection" isPopular={false} productlink={"https://buy.stripe.com/test_7sI28cfd6fzH2TC6oq"} /> */}
         <PricingCards
-          num="€39.30"
+          num="€39.90"
           des="Unlimited"
           point1="Access to unlimited modifications and reports"
           point2="Receive three free consultations with our consultants"
