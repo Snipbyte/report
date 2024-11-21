beb643d9b971c65ce4d974d4f3e4a29ea621fb93
Snipbyte <snipbytesol@gmail.com>
2024-11-21 08:50:42 +0500

    test

--- src/app/components/admin/contentManagement/landingPage/landingMainPage/landingMainPage.js
+++ src/app/components/admin/contentManagement/landingPage/landingMainPage/landingMainPage.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import HeroSection from '../heroSection/heroSection'
-import SimulationCard from '../simulationCard/simulationCard'
-import SectionCard from '../sectionCard/sectionCard'
-import GuidanceCard from '../guidanceCard/guidanceCard'
-import WorksCard from '../worksCard/worksCard'
-import ChooseCard from '../chooseCard/chooseCard'
-import AmbitionCard from '../ambitionCard/ambitionCard'
-import Testimonials from '../testimonials/testimonials'
+import React from "react";
+import HeroSection from "../heroSection/heroSection";
+import SimulationCard from "../simulationCard/simulationCard";
+import SectionCard from "../sectionCard/sectionCard";
+import GuidanceCard from "../guidanceCard/guidanceCard";
+import WorksCard from "../worksCard/worksCard";
+import ChooseCard from "../chooseCard/chooseCard";
+import AmbitionCard from "../ambitionCard/ambitionCard";
+import Testimonials from "../testimonials/testimonials";
 
 const LandingMainPage = () => {
-    return (
-        <div className='p-2'>
-            <HeroSection />
-            <SimulationCard />
-            <SectionCard />
-            <GuidanceCard />
-            <WorksCard />
-            <ChooseCard/>
-            <AmbitionCard/>
-            <Testimonials/>
-        </div>
-    )
-}
+  return (
+    <div className="p-2">
+      <HeroSection />
+      <SimulationCard />
+      <SectionCard />
+      <GuidanceCard />
+      <WorksCard />
+      <ChooseCard />
+      <AmbitionCard />
+      <Testimonials />
+      <button className="p-2 bg-blue-500 rounded text-white hover:rounded-lg">submit</button>
+    </div>
+  );
+};
 
-export default LandingMainPage
+export default LandingMainPage;
