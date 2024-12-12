f708957f2b2b81dba1192ffd4baf9ba75ca02929
moizsheraz <user146712@gmail.com>
2024-12-12 11:02:57 +0500

    feat:updates

---
+++ src/app/api/admin/plans/getAll/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import dbConnect from "../../../../../../backend/middleware/db";
+import Plan from "../../../../../../backend/models/plans";
+
+const getAllPlans = async () => {
+    try {
+      const plans = await Plan.find();
+      return NextResponse.json({ success: true, data: plans }, { status: 200 });
+    } catch (error) {
+      console.error("Error fetching plans:", error);
+      return NextResponse.json({ message: "Failed to fetch plans." }, { status: 500 });
+    }
+  };
+
+  export const GET = dbConnect(getAllPlans);
